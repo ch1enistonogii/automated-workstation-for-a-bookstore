b8f2691c0ad7496a465f563e1ab4ba04b4a45824
ch1enistonogii <roma228png@gmail.com>
2024-01-24 23:43:18 +0300

    Добавьте файлы проекта.

---
+++ automated-workstation-for-a-bookstore/login.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace automated_workstation_for_a_bookstore
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            textBoxIP = new TextBox();
+            groupBox2 = new GroupBox();
+            textBoxPort = new TextBox();
+            groupBox3 = new GroupBox();
+            textBoxDatabase = new TextBox();
+            groupBox4 = new GroupBox();
+            textBoxUser = new TextBox();
+            groupBox5 = new GroupBox();
+            textBoxPassword = new TextBox();
+            CheckConnectionButton = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBoxIP);
+            groupBox1.Location = new Point(45, 89);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(261, 63);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Путь";
+            // 
+            // textBoxIP
+            // 
+            textBoxIP.Location = new Point(6, 22);
+            textBoxIP.Name = "textBoxIP";
+            textBoxIP.Size = new Size(249, 23);
+            textBoxIP.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBoxPort);
+            groupBox2.Location = new Point(45, 158);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(261, 63);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Порт";
+            // 
+            // textBoxPort
+            // 
+            textBoxPort.Location = new Point(6, 22);
+            textBoxPort.Name = "textBoxPort";
+            textBoxPort.Size = new Size(249, 23);
+            textBoxPort.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBoxDatabase);
+            groupBox3.Location = new Point(45, 227);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(261, 63);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "База данных";
+            // 
+            // textBoxDatabase
+            // 
+            textBoxDatabase.Location = new Point(6, 22);
+            textBoxDatabase.Name = "textBoxDatabase";
+            textBoxDatabase.Size = new Size(249, 23);
+            textBoxDatabase.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(textBoxUser);
+            groupBox4.Location = new Point(45, 296);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(261, 63);
+            groupBox4.TabIndex = 2;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Пользователь";
+            // 
+            // textBoxUser
+            // 
+            textBoxUser.Location = new Point(6, 22);
+            textBoxUser.Name = "textBoxUser";
+            textBoxUser.Size = new Size(249, 23);
+            textBoxUser.TabIndex = 1;
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(textBoxPassword);
+            groupBox5.Location = new Point(45, 365);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(261, 63);
+            groupBox5.TabIndex = 2;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "Пароль";
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Location = new Point(6, 22);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(249, 23);
+            textBoxPassword.TabIndex = 1;
+            // 
+            // CheckConnectionButton
+            // 
+            CheckConnectionButton.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            CheckConnectionButton.Location = new Point(45, 434);
+            CheckConnectionButton.Name = "CheckConnectionButton";
+            CheckConnectionButton.Size = new Size(261, 63);
+            CheckConnectionButton.TabIndex = 3;
+            CheckConnectionButton.Text = "Проверить соединение";
+            CheckConnectionButton.UseVisualStyleBackColor = true;
+            CheckConnectionButton.Click += CheckConnectionButton_Click;
+            // 
+            // login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(808, 546);
+            Controls.Add(CheckConnectionButton);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox1);
+            Name = "login";
+            Text = "login";
+            Load += login_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            groupBox5.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBoxIP;
+        private GroupBox groupBox2;
+        private TextBox textBoxPort;
+        private GroupBox groupBox3;
+        private TextBox textBoxDatabase;
+        private GroupBox groupBox4;
+        private TextBox textBoxUser;
+        private GroupBox groupBox5;
+        private TextBox textBoxPassword;
+        private Button CheckConnectionButton;
+    }
+}
---
+++ automated-workstation-for-a-bookstore/login.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace automated_workstation_for_a_bookstore
+{
+    public partial class login : Form
+    {
+        NpgsqlConnection connection = new NpgsqlConnection();
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private void OpenMenu()
+        //  Закрытие текущей формы и открытие новой
+        {
+            catalog catalogForm = new catalog(connection);
+            this.Hide();
+            catalogForm.FormClosed += (s, args) => this.Close();
+            catalogForm.Show();
+        }*/
+
+        private NpgsqlConnection CreateConnection()
+        // Функция создает подключение к базе данных
+        {
+            try
+            {
+                string configFilePath = "config.txt";
+                string[] lines = File.ReadAllLines(configFilePath);
+                string connectionString = $"Server={textBoxIP.Text};Port={textBoxPort.Text};Database={textBoxDatabase.Text};User Id={textBoxUser.Text};Password={textBoxPassword.Text}";
+                return new NpgsqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при создании подключения из файла: {ex.Message}");
+                return null;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadConfig()
+        //  Функция загружает данные прердыдущего успешного подключения из config.txt, и использует их при подключении
+        {
+            string configFilePath = "config.txt";
+
+            if (File.Exists(configFilePath) && File.ReadAllLines(configFilePath).Length > 0)
+            {
+                try
+                {
+                    string[] lines = File.ReadAllLines(configFilePath);
+                    textBoxIP.Text = lines[0];
+                    textBoxPort.Text = lines[1];
+                    textBoxDatabase.Text = lines[2];
+                    textBoxUser.Text = lines[3];
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при чтении данных из файла: {ex.Message}");
+                }
+            }
+        }
+
+        private void SaveConfigData()
+        //  Функция обновляет данные дял подключение в файле config.txt
+        {
+            string configFilePath = "config.txt";
+
+            try
+            {
+                File.WriteAllText(configFilePath, $"{textBoxIP.Text}\n{textBoxPort.Text}\n{textBoxDatabase.Text}\n{textBoxUser.Text}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении данных в файле конфигурации: {ex.Message}");
+            }
+        }
+
+        private void CheckConnectionButton_Click(object sender, EventArgs e)
+        //  Кнопка создает подключение, после чего открывает его и выводит статус
+        {
+            {
+                connection = CreateConnection();
+                try
+                {
+                    if (connection.State != ConnectionState.Open)
+                    {
+                        connection.Open();
+                    }
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        SaveConfigData();
+                        MessageBox.Show("Подключение установлено!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Не удалось установить подключение к базе данных.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}");
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                        //OpenMenu();
+                    }
+                }
+            }
+        }
+
+    }
+}
